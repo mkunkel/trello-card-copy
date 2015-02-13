23271d2a9a03acaa5b85ab9a376ab264ac64b219
Mike Kunkel <mike@mkunkel.com>
2015-02-13 16:05:41 -0600

    switch to rawgit CDN

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,9 +1,9 @@
 $("<link/>", {
    rel: "stylesheet",
    type: "text/css",
-   href: "https://rawgit.com/mkunkel/trello-card-copy/master/style.css"
+   href: "https://cdn.rawgit.com/mkunkel/trello-card-copy/master/style.css"
 }).appendTo("head");
-$.getScript('https://rawgit.com/mkunkel/trello-card-copy/master/script.min.js', function() {
+$.getScript('https://cdn.rawgit.com/mkunkel/trello-card-copy/master/script.min.js', function() {
   var $link = $('<a>').attr('href', 'javascript:$(".progress-report").slideUp(600,function(){$(".progress-report").remove()});').text('Close').attr('id', 'close-link');
   $link.css('color', '#FFFFFF').css('position', 'absolute').css('bottom', '10px').css('right', '10px');
   $link.css('font-size', '14pt');
