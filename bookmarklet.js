447a3731c4a33dff628b42e8329b5ce65e5ba8fd
Mike Kunkel <mike@mkunkel.com>
2015-02-13 16:00:44 -0600

    Clean up formatting

--- bookmarklet.js
+++ bookmarklet.js
@@ -8,13 +8,10 @@ $.getScript('https://rawgit.com/mkunkel/trello-card-copy/master/script.min.js', 
   $link.css('color', '#FFFFFF').css('position', 'absolute').css('bottom', '10px').css('right', '10px');
   $link.css('font-size', '14pt');
   var output = [];
-  output.push('<p>Notable work from last week</p>')
-  output.push(getOutput('Done', 'Trello'));
-  output.push('<p>Notable work planned this week</p>')
-  output.push(getOutput('To Do', 'Trello'));
-  output.push(getOutput('Doing', 'Trello'));
-  output.push('<p>Open questions</p>')
-  output.push(getOutput('Questions', 'Discussion Here'));
+  output.push('<br>');
+  output.push('<p>*Notable work from last week*<br>' + getOutput('Done', 'Trello') + '</p>');
+  output.push('<p>*Notable work planned this week*' + getOutput('To Do', 'Trello') + '<br>' + getOutput('Doing', 'Trello') + '</p>');
+  output.push('<p>*Open questions*<br>' + getOutput('Questions', 'Discussion Here') + '</p>');
 
   var $div = $('<div>').html(output.join('<br>')).addClass('progress-report');
   var left = ($(window).width() - 1200) / 2 + 'px';
