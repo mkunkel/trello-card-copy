0ceb6727262f7ddb24421fca53ba24ecae4ac916
Mike Kunkel <mike@mkunkel.com>
2015-02-13 15:01:15 -0600

    slide in/out and moving the link

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,11 +4,12 @@ $("<link/>", {
    href: "https://rawgit.com/mkunkel/trello-card-copy/master/style.css"
 }).appendTo("head");
 $.getScript('https://rawgit.com/mkunkel/trello-card-copy/master/script.min.js', function() {
-  var $link = $('<a>').attr('href', 'javascript:$div.remove();').text('Close').attr('id', 'close-link');
-  $link.css('float', 'right').css('color', '#FFFFFF');
+  var $link = $('<a>').attr('href', 'javascript:$(".progress-report").slideUp(600,function(){$(".progress-report").remove()});').text('Close').attr('id', 'close-link');
+  $link.css('color', '#FFFFFF').css('position', 'absolute').css('bottom', '10px').css('right', '10px');
+  $link.css('font-size', '14pt');
   var $div = $('<div>').html(getOutput).addClass('progress-report');
   var left = ($(window).width() - 1200) / 2 + 'px';
   $div.css('left', left).append($link);
   $('body').append($div);
-  $div.hide().fadeIn();
+  $div.hide().slideDown(600);
 });
