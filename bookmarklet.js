15ba1f27be9c79882b23daaf52c169661ef24d07
Mike Kunkel <mike@mkunkel.com>
2015-02-13 14:45:54 -0600

    set div to 1200px

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,14 +1,14 @@
 $("<link/>", {
    rel: "stylesheet",
    type: "text/css",
-   href: "https://cdn.rawgit.com/mkunkel/trello-card-copy/master/style.css"
+   href: "https://rawgit.com/mkunkel/trello-card-copy/master/style.css"
 }).appendTo("head");
-$.getScript('https://rawgit.com/mkunkel/trello-card-copy/master/script.min.js')
-
-
-var $link = $('<a>').attr('href', 'javascript:$div.remove();').text('Close').attr('id', 'close-link');
-$link.css('float', 'right').css('color', '#FFFFFF');
-var $div = $('<div>').html(getOutput).addClass('progress-report');
-$('body').append($div);
-var left = ($(window).width() - $div.outerWidth()) / 2 + 'px';
-$div.css('left', left).append($link);
+$.getScript('https://rawgit.com/mkunkel/trello-card-copy/master/script.min.js', function() {
+  var $link = $('<a>').attr('href', 'javascript:$div.remove();').text('Close').attr('id', 'close-link');
+  $link.css('float', 'right').css('color', '#FFFFFF');
+  var $div = $('<div>').html(getOutput).addClass('progress-report');
+  var left = ($(window).width() - 1200) / 2 + 'px';
+  $div.css('left', left).append($link);
+  $('body').append($div);
+  $div.hide().fadeIn();
+});
